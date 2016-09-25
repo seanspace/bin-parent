c36189c3a10838b2e2fc022d9a1fde5375a34b9f
xiaobin <xbliu564@163.com>
2016-09-25 20:40:33 +0800

    添加工具类

---
+++ bin-redis/src/main/java/com/bin/redis/utils/JedisUtil.java
@@ -0,0 +1,31 @@
+package com.bin.redis.utils;
+
+import org.apache.commons.lang.StringUtils;
+import redis.clients.jedis.Jedis;
+
+/**
+ * 简单的链接获取，不可用于生产
+ */
+public class JedisUtil {
+
+    public static Jedis createJedis() {
+        Jedis jedis = new Jedis("192.168.163.129",6379);
+        jedis.auth("123");
+        return jedis;
+    }
+
+    public static Jedis createJedis(String host, int port) {
+        Jedis jedis = new Jedis(host, port);
+
+        return jedis;
+    }
+
+    public static Jedis createJedis(String host, int port, String passwrod) {
+        Jedis jedis = new Jedis(host, port);
+
+        if (!StringUtils.isNotBlank(passwrod))
+            jedis.auth(passwrod);
+        
+        return jedis;
+    }
+}
